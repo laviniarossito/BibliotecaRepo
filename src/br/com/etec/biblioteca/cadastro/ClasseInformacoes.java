b8d81dd59d3ca6e31a2a3ff10e96de713dfbec14
laviniarossito <laviniarossito.etec@gmail.com>
2025-10-03 10:37:31 -0300

    FINALIZADO: ClasseInformacoes

--- src/br/com/etec/biblioteca/cadastro/ClasseInformacoes.java
+++ src/br/com/etec/biblioteca/cadastro/ClasseInformacoes.java
@@ -2,4 +2,74 @@ package br.com.etec.biblioteca.cadastro;
 
 public class ClasseInformacoes {
 
+	//Atributos
+	String titulosAt;
+	String subtitulosAt;
+	int numerosPaginasAt;
+	String autoresAt;
+	String generosAt;
+	
+	//Construtor vazio
+	public ClasseInformacoes(){
+		
+	}
+	
+	//Construtor 
+	public ClasseInformacoes(String autorPar, String tituloPar){
+		this.autoresAt = autorPar;
+		this.titulosAt = tituloPar;
+	}
+	
+	//Getters
+	public String getTitulos(){
+		return titulosAt;
+	}
+	
+	public String getSubtitulos(){
+		return subtitulosAt;
+	}
+
+
+	public int getNumerosPaginas(){
+		return numerosPaginasAt;
+	}
+	
+	public String getAutores(){
+		return autoresAt;
+	}
+
+	public String getGenero(){
+		return generosAt;
+	}
+
+	//Setters
+	public void setTitulos(String titulosPar){
+		this.titulosAt = titulosPar;
+	}
+
+	public void setSubtitulos(String subtitulosPar){
+		this.subtitulosAt = subtitulosPar;
+	}
+
+	public void setNumerosPaginas(int numerosPaginasPar){
+		this.numerosPaginasAt = numerosPaginasPar;
+	}
+
+	public void setAutores(String autoresPar){
+		this.autoresAt = autoresPar;
+	}
+
+	public void setGenero(String generosPar){
+		this.generosAt = generosPar;
+	}
+
+	//Método próprio
+	public void mostrarDados(){
+		System.out.println("Informações do produto:");
+		System.out.println(titulosAt);
+		System.out.println(subtitulosAt);
+		System.out.println(numerosPaginasAt);
+		System.out.println(autoresAt);
+		System.out.println(generosAt);
+	}
 }
