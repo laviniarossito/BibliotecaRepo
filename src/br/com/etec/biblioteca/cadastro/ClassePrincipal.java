bb9e374cf0ccd38635c7475dfe39d3f777af5e14
laviniarossito <laviniarossito.etec@gmail.com>
2025-10-03 10:47:28 -0300

    FINALIZADO: ClasseInformacos e ADICIONADO: Objetos na ClassePrincipal

--- src/br/com/etec/biblioteca/cadastro/ClassePrincipal.java
+++ src/br/com/etec/biblioteca/cadastro/ClassePrincipal.java
@@ -3,8 +3,11 @@ package br.com.etec.biblioteca.cadastro;
 public class ClassePrincipal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		ClasseInformacoes info_Obj1 = new ClasseInformacoes();
+		ClasseInformacoes info_Obj2 = new ClasseInformacoes("Dom Casmurro", "Machado de Assis");
+		info_Obj1.mostrarDados(); //null 0
+		info_Obj2.mostrarDados(); //null 0 autor e titulo
 	}
 
 }
